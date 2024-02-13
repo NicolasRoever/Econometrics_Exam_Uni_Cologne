0eefdd83d47e3a86ad78712be02ac4e938e6e0cb
NicolasRoever <nicoroever@gmail.com>
2024-02-13 14:20:32 +0100

    Examanwers.

--- main_question1_exam_2024.R
+++ main_question1_exam_2024.R
@@ -1,5 +1,5 @@
 ################################################################################
-##Code for Question 1 in the Exam on January 5th, 2024 in Advanced Econometrics###
+##Code for Question "Milk" in the Exam on January 5th, 2024 in Advanced Econometrics###
 ################################################################################
 
 ################################################################################
@@ -33,5 +33,70 @@ check_if_columns_are_numeric_or_integer(df)
 
 check_for_nas_in_df(df)
 
+df <- df %>%
+  mutate(log_Sales = log(SALE * (7354299/7123456)))
+
+
 ################################################################################
 ##Task a)
+
+demand_model <- lm(log_Sales ~ log(PRICE) + log(PRICE_FM) + log(PROMOTION), data = df)
+
+car::durbinWatsonTest(demand_model, max.lag= 4) 
+
+#The errors are highly autocorrelated! Every lag up until the fourth has a p-value
+#very close to 0. 
+
+coeftest(demand_model)
+
+################################################################################
+##Task b)
+
+approximate_effect <- demand_model$coefficients["log(PRICE)"]
+
+approximate_effect
+
+#Approximately, sales decrease by -3.281473% 
+
+exact_effect = exp(demand_model$coefficients["log(PRICE)"]*log(1.01))
+
+1- exact_effect
+
+#Exact effect, is given by -0.03212443 
+
+################################################################################
+##Task c)
+
+linearHypothesis(demand_model, c("log(PRICE) + log(PRICE_FM) = 0"))
+
+#The p-value is 0.3369, so I cannot reject the null-hypothesis on a reasonable 
+#significance level. 
+
+#I would reformulate the following way: Only include the variable log(PRICE) - log(PRICE_FM);
+#so I would not include log(PRICE) and log(PRICE_FM) separately. This way, the model 
+#represents our hypothesis that the coeffecients sum up to zero (this can be derived
+#using simple algebra, just plugging in -beta_{log_Price} for beta_{log_Price_FM} in 
+#the model specifcitation.
+
+################################################################################
+##Task d)
+
+df <- df %>%
+  mutate(log_price_squared = log(PRICE)^2, 
+         log_price = log(PRICE))
+
+quadratic_model <-lm(log_Sales ~ log_price + log_price_squared, data = df)
+
+summary(quadratic_model)
+
+minimum <- quadratic_model$coefficients["log_price"]/(2*quadratic_model$coefficients["log_price_squared"])
+
+minimum_real_number <- exp(minimum)
+
+minimum_real_number
+
+#Note that this is a minimum, because the coefficient of the squared term is positive
+
+linearHypothesis(quadratic_model , c("log_price - log_price_squared = 0"))
+
+#I can reject that the minimum sales are at 1 euro at any reasonable significance level.
--- main_question_2_exam_2024.R
+++ main_question_2_exam_2024.R
@@ -1,5 +1,5 @@
 ################################################################################
-##Code for Question 2 in the Exam on January 5th, 2024 in Advanced Econometrics###
+##Code for Question "Unemployment" in the Exam on January 5th, 2024 in Advanced Econometrics###
 ################################################################################
 
 ################################################################################
@@ -19,12 +19,13 @@ library(lmtest)
 library(car)
 library(tseries)
 library(dynlm)
+library(pscl)
 source(path(MAIN_DIRECTORY_PATH, "functions_exam_2024.R"))
 
 ################################################################################
 ##Load and Clean Data
 
-Data_Path = list.files(path = path(MAIN_DIRECTORY_PATH, "Data", "Question_1"),
+Data_Path = list.files(path = path(MAIN_DIRECTORY_PATH, "Data", "Question_2"),
                        pattern = ".csv", full.names = TRUE)
 
 df <- import(Data_Path) 
@@ -33,5 +34,75 @@ check_if_columns_are_numeric_or_integer(df)
 
 check_for_nas_in_df(df)
 
+constant <- 5/(1+5)
+
+df = df %>%
+  mutate(age = age + constant) %>%
+  na.omit()
+
 ################################################################################
 ##Task a)
+
+lpm <- lm(unemployed ~ age + age^2+ educ + health +
+            female + migback, data = df)
+
+
+#I assume heteroskedasticity for the error dependent on the value of unemployed. 
+#The reason is that unemployed follows a bernoulli distribution, which means the 
+#variance of the error depends on the values of the dependent variables.
+
+coeftest(lpm)
+
+################################################################################
+##Task b)
+
+estimations <- lpm$fitted.values
+
+hist(estimations, breaks = 50, main = "Histogram of Estimations", xlab = "Estimations")
+
+#In the plot, we observe negativ values for the probability to be unemployed. The reason
+#is that the linear probability model does not restrict the values of the dependent variable
+#to be between 0 and 1. This is a problem, because the dependent variable is a probability
+#which can only be between 0 and 1 (see kolmogorovs definition of probabilities.)
+#A solution is to use a probit or logit model, which restricts the range of values
+#through the choice of the link function.
+
+################################################################################
+##Task c)
+
+mean(df$unemployed)
+
+#Unconditionally, the probability is 5%. 
+
+coefficient_age = lpm$coefficients['age']
+
+coefficient_age * 5
+
+# the probability decreases by -0.008623624. 
+
+
+################################################################################
+##Task d)
+
+probit_model <- glm(unemployed ~ age + age^2+ educ + health +
+                      female + migback, data = df, family = binomial(link = "probit"))
+
+summary(probit_model)
+
+fit_measures <- pR2(probit_model, type = "McFadden")
+
+fit_measures["McFadden"]
+
+df["probit_estimations"] <- predict(probit_model, type = "response")
+
+extra_regression <- lm(unemployed ~ probit_estimations, data = df)
+
+summary(extra_regression)
+
+#The pseudo R^2 is higher for the probit model than when using the approach of 
+#first estimating the predicted values and then using a normal R^2 from a linear 
+#regression. This highlights that pseudo-R^2 and R^2 are different measures to 
+#assess model fit. Both measures focus on different aspects of the model fit and
+#thus do not give equal results. However, results for both measures should 
+#always be in the same ballpark, as these measures try to assess the same thing 
+#and are not that different. 
